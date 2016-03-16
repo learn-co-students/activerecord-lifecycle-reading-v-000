1cd5601b041565f5f1b679eb754d17714b3c41fc
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-03-16 22:47:17 +0000

    completed.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
+ 
   private
 
   def is_title_case
@@ -16,3 +17,7 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
 end
+
+
+  
+ 
