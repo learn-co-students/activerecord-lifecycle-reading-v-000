2fec52e334f381ae87436948d0559b1de968fe24
Lady Codington <myemailforcoding@gmail.com>
2016-04-05 20:48:29 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
-  private
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
