1818bb2b8e5749c5ff31ea3b3f04de379e46b10f
ldgarber <leah.garber88@gmail.com>
2016-07-25 16:12:10 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,12 @@ class Post < ActiveRecord::Base
   validate :is_title_case 
   before_validation :make_title_case 
   belongs_to :author
+  before_save :email_author_about_post
 
   private
+
+  def email_author_about_post
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
