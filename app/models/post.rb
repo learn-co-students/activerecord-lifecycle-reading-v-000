5ff257b4c65fe05832e00b9bbba90fa0d5c842ae
krisnotchris <krishenderson@gmail.com>
2016-07-24 22:20:48 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ActiveRecord::Base
   validate :is_title_case 
   before_validation :make_title_case 
   belongs_to :author
+  before_save :email_author_about_post
 
   private
 
@@ -14,5 +15,8 @@ class Post < ActiveRecord::Base
 
   def make_title_case
     self.title = self.title.titlecase
+  end
+
+  def email_author_about_post
   end
 end
