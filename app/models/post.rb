384524aad395002667ccb8cc31fec47f6e7f87ca
ryanwevans <ryanwevans@icloud.com>
2018-06-08 14:52:06 -0700

    add lifecycle callbacks to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  # Example of use for before_save callback. It is used for actions that need to occur that aren't modifying the model itself:
+  # before_save :email_author_about_post
 
   private
 
