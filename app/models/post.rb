e8a31ea2dcd45c628b904bcc2dcc6dcbed9f0da1
Maverick Tesini <mavericktesini@gmail.com>
2018-07-31 00:14:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  # before_save :make_title_case #occurs just before save to db.
+  before_validation :make_title_case #occurs before BOTH validation and save to db.
+  # before_save :email_author_about_post #author is alerted after validation and just prior to db alteration.
+  #before_create will run similarly to before_save, but only upon the intitial creation of an object.
 
   private
 
