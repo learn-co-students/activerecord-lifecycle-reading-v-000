1212b69196e84165e8ac7a6153c65dbb801d8410
mseaman6 <monicaameeti@gmail.com>
2018-09-26 15:04:45 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,11 @@
 class Post < ActiveRecord::Base
 
   belongs_to :author
-  validate :is_title_case 
+  validate :is_title_case
+
+  before_validation :make_title_case
+
+  #before_save :email_author_about_post or other action that doesn't modify the model
 
   private
 
