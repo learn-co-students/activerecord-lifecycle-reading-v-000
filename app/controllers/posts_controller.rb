c17805a6c174c511e1ddc85d430fa0be17465c32
Leslie Stevens <flymaven@protonmail.com>
2019-03-19 17:48:24 -0500

    add callbacks, fix a lot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,13 @@
 class PostsController < ApplicationController
+
+	before_action :set_post!, only:[:show, :edit, :update]
+
 	def index
 		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
+		#@post = Post.find(params[:id])
 	end
 
 	def new
@@ -12,18 +15,30 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params)
+	  @post = Post.new(post_params)
 	  @post.save
 	  redirect_to post_path(@post)
 	end
 
 	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post))
+	  #@post = Post.find(params[:id])
+	  @post.update(post_params)
 	  redirect_to post_path(@post)
 	end
 
 	def edit
-	  @post = Post.find(params[:id])
+	  #@post = Post.find(params[:id])
 	end
+
+
+private
+
+	def post_params
+		params.require(:post).permit(:title, :description, :post_status)
+	end
+
+	def set_post!
+		@post = Post.find(params[:id])
+	end
+
 end
