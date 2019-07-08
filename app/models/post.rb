518ba75dc05b56591b847f38fc9cbcb02f206d22
github username <github email address>
2019-07-08 11:14:57 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,17 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  # New Code!!
+  before_validation :make_title_case 
+  # Whenever you are modifying an attribute of the model, use before_validation. If you are doing some other action, then use before_save.
+
+  #before_save :email_author_about_post
+
+  # This is before create
+  #before_create do
+  #  self.name = login.capitalize if name.blank?
+  #end
+  
   private
 
   def is_title_case
